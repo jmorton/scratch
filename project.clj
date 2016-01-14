7b7a188ecb72b714968e2baa6c5cf53f3d9d3c90
Jonathan Morton <jmorton@element84.com>
2016-01-14 10:21:37 -0600

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject scratch "0.1.0-SNAPSHOT"
+  :description "Trying new things"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]])
